3a65fba5cfaedad8c62e340b5f631353657a540d
mura-s <seiya.muramatsu1@gmail.com>
2014-10-25 00:25:30 +0900

    メモの一覧を作成

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
 window.App = {};
+
+$(function() {
+  var noteCollection = new App.NoteCollection([{
+    title: 'test1',
+    body: 'test1 body'
+  }, {
+    title: 'test2',
+    body: 'test2 body'
+  }]);
+
+  var mainContainer = new App.Container({
+    el: '#main-container'
+  });
+
+  var noteListView = new App.NoteListView({
+    collection: noteCollection
+  });
+
+  mainContainer.show(noteListView);
+});

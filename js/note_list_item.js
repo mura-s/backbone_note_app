848334741ff60592e61b72fb6c6a20a09b35766b
mura-s <seiya.muramatsu1@gmail.com>
2014-10-25 11:10:16 +0900

    削除機能を追加

--- js/note_list_item.js
+++ js/note_list_item.js
@@ -1,11 +1,23 @@
 App.NoteListItemView = Backbone.View.extend({
   tagName: 'tr',
 
+  initialize: function() {
+    this.listenTo(this.model, 'destroy', this.remove);
+  },
+
+  events: {
+    'click .js-delete': 'onClickDelete'
+  },
+
   render: function() {
     var template = $('#noteListItemView-template').html();
     var compiled = _.template(template);
     var html = compiled(this.model.toJSON());
     this.$el.html(html);
     return this;
+  },
+
+  onClickDelete: function() {
+    this.model.destroy();
   }
 });
